08135995ba33fc382242bcee1b4aecc8cf3454e3
Kay-Rohit <rohit.apex1999@gmail.com>
2023-03-13 17:16:06 +0530

    added configuration for log4j and generating log files using log4j2.xml

--- mini_project/src/main/java/com/spe/project/Calculator.java
+++ mini_project/src/main/java/com/spe/project/Calculator.java
@@ -5,10 +5,11 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 public class Calculator {
 
-//	private static final Logger logger = LogManager.getLogger(Calculator.class);
+	private static final Logger logger = LogManager.getLogger(Calculator.class);
 
     //my factorial function
     static long factorial(int n){
+		logger.info("[FACTORIAL] - " + n);
         if (n == 0)
             return 1;
         else
@@ -16,10 +17,16 @@ public class Calculator {
     }
 
 	static double square_root(int n){
-		return Math.sqrt(n);
+		double result;
+		logger.info("[SQ ROOT] - " + n);
+
+		result = Math.sqrt(n);
+		logger.info("[RESULT - SQ ROOT] - " + result);
+		return result;
 	}
 
 	static double natural_log(int n){
+		logger.info("[NATURAL LOG] - " + n);
 		double result=0;
 		try {
 			if (n < 0 ) {
@@ -33,14 +40,19 @@ public class Calculator {
 				result = Math.log(n);
 			}
 		} catch (ArithmeticException error) {
+			logger.error("[EXCEPTION - LOG] - Cannot find log of negative numbers");
 			System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
 		}
-		System.out.println("[RESULT - NATURAL LOG] - " + result);
+		logger.info("[RESULT - NATURAL LOG] - " + result);
 		return result;
 	}
 
 	static double x_power_b(int x, int b){
-		return Math.pow(x, b);
+		logger.info("[POWER of" + x + "RAISED TO]" + b);
+		double res=0;
+		res = Math.pow(x, b);
+		logger.info("[RESULT - POWER] - " + res);
+		return res;
 	}
     public static void main(String a[])
 	{
